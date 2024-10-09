53d39b5967ebe724134f4f0126a5c11f6f1606a8
Daiani Noronha <daianinoronhadev@gmail.com>
2024-10-09 10:45:44 -0300

    initial commit

---
+++ Controllers/LivrariaController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APILivraria.Controllers;
+[Route("livraria/[controller]")]
+[ApiController]
+public class LivrariaController : ControllerBase
+{
+    
+    private static List<Livro> livros = new List<Livro>
+        {
+            new Livro 
+            { 
+                Id = 1, 
+                Titulo = "O Príncipe Cruel", 
+                Autor = "Holly Black", 
+                Genero = Genero.Fantasia.ToString(), 
+                Preco = 25.50M, 
+                Estoque = 10 
+            },
+
+            new Livro
+
+            {
+                Id = 2,
+                Titulo = "A Hipóste do Amor",
+                Autor = "Ale Hazelwood",
+                Genero = Genero.Romance.ToString(),
+                Preco = 45M,
+                Estoque = 5
+            },
+    };
+
+    
+    [HttpGet]
+    [ProducesResponseType(typeof(IEnumerable<Livro>), StatusCodes.Status200OK)]
+    public IActionResult GetAll()
+    {
+        return Ok(livros);
+    }
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(typeof(Livro), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public IActionResult GetById(int id)
+    {
+        var livro = livros.FirstOrDefault(l => l.Id == id);
+        if (livro == null)
+        {
+            return NotFound("Livro não encontrado.");
+        }
+
+        return Ok(livro);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(Livro), StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public IActionResult Create([FromBody] Livro request)
+    {
+        if (request == null)
+        {
+            return BadRequest("Livro inválido.");
+        }
+
+        request.Id = livros.Count + 1; 
+        livros.Add(request);
+
+        return CreatedAtAction(nameof(GetById), new { id = request.Id }, request);
+    }
+
+    [HttpPut]
+    [Route("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+    public IActionResult Update(int id, [FromBody] Livro request)
+
+    { if (request == null)
+        {
+            return BadRequest("Update inválido.");
+        }
+
+        var livroExistente = livros.FirstOrDefault(l => l.Id == id);
+        if (livroExistente == null)
+        {
+            return NotFound("Livro não encontrado.");
+        }
+
+        livroExistente.Titulo = request.Titulo;
+        livroExistente.Autor = request.Autor;
+        livroExistente.Genero = request.Genero;
+        livroExistente.Preco = request.Preco;
+        livroExistente.Estoque = request.Estoque;
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public IActionResult Delete(int id)
+    {
+        var livroExistente = livros.FirstOrDefault(l => l.Id == id);
+        if (livroExistente == null)
+        {
+            return NotFound("Livro não encontrado.");
+        }
+
+        livros.Remove(livroExistente);
+
+        return NoContent();
+    }
+}
---
+++ Livro.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace APILivraria
+{
+
+    public enum Genero
+    {
+        Romance,
+        Fantasia,
+        Romantasia,
+    }
+
+
+    public class Livro
+    {
+        public int Id { get; set; }
+
+        public string Titulo { get; set; } = string.Empty;
+
+        public string Autor { get; set; } = string.Empty;
+
+        public string Genero { get; set; } = string.Empty;
+
+        public decimal Preco { get; set; }
+
+        public int Estoque { get; set; }
+    }
+}
+
+
+
